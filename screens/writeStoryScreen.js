9d4f6b6042219a09be9fe2b5ad19251ca4d215c7
Sreshta0603 <67061788+ssreshta0603@users.noreply.github.com>
2021-04-22 13:42:53 +0530

    Add files via upload

---
+++ screens/writeStoryScreen.js
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { Header } from "react-native-elements";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+export default class WriteStoryScreen extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#white",
+        }}
+      >
+        <Header
+          backgroundColor={"#f38181"}
+          centerComponent={{
+            text: "Story Hub",
+            style: { fontSize: 28, color: "white" },
+          }}
+        />
+        <TextInput style={styles.inputBox} placeholder="Story Title" />
+        <TextInput style={styles.inputBox} placeholder="Author" />
+        <TextInput
+          style={styles.inputBoxMultiline}
+          placeholder="Write your Story"
+          multiline={true}
+
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputBox: {
+    width: "90%",
+    height: 50,
+    borderWidth: 2,
+    margin: 20,
+    padding: 5,
+    borderRadius : 50
+  },
+  inputBoxMultiline: {
+    width: "90%",
+    height: "35%",
+    borderWidth: 2,
+    margin: 20,
+    padding: 5,
+  },
+  button: {
+    margin: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#95e1d3",
+    width: 200,
+    height: 50,
+    alignSelf: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
